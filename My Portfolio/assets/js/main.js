52ec660d20b3d350008c4f50d855f84c6dcebd1a
Gokulmani <143214038+Gokulmani-P@users.noreply.github.com>
2024-06-13 21:36:17 +0530

    Update main.js

--- My Portfolio/assets/js/main.js
+++ My Portfolio/assets/js/main.js
@@ -1,36 +1,55 @@
-/*=============== SHOW SIDEBAR ===============*/
+let btnMenu = document.getElementById('btn-menu');
+let Menu = document.querySelector('.nav-links');
 
+btnMenu.onclick = function() {
+    btnMenu.classList.toggle('fa-times');
+    Menu.classList.toggle('active');
+}
 
-/*===== SIDEBAR SHOW =====*/
-/* Validate If Constant Exists */
+let header = document.querySelector('header');
 
+window.onscroll = function() {
+    if (this.scrollY >= 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+    btnMenu.classList.remove('fa-times');
+    Menu.classList.remove('active');
+}
 
-/*===== SIDEBAR HIDDEN =====*/
-/* Validate If Constant Exists */
+// Typewriting effect
+const typedTextSpan = document.querySelector(".typewriting-text");
 
+const textArray = ["Front-End Developer","Web Developer"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 1000;
+let textArrayIndex = 0;
+let charIndex = 0;
 
-/*=============== SKILLS TABS ===============*/
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
 
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1000);
+    }
+}
 
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
-
-/*===== Link Active Work =====*/
-
-
-/*===== Work Popup =====*/
-
-
-/*=============== SERVICES MODAL ===============*/
-
-
-/*=============== SWIPER TESTIMONIAL ===============*/
-
-
-/*=============== INPUT ANIMATION ===============*/
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/
+document.addEventListener("DOMContentLoaded", function() {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
